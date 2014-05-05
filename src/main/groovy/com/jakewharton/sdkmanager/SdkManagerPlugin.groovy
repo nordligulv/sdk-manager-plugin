23b087057e2da5972cf6d00dd0f8aacae19b9745
Andrei Korzhevskii <a.korzhevskiy@gmail.com>
2014-05-05 13:21:39 +0400

    Update SdkManagerPlugin.groovy

--- src/main/groovy/com/jakewharton/sdkmanager/SdkManagerPlugin.groovy
+++ src/main/groovy/com/jakewharton/sdkmanager/SdkManagerPlugin.groovy
@@ -41,9 +41,9 @@ class SdkManagerPlugin implements Plugin<Project> {
   }
 
   def time(String name, Closure task) {
-    long before = java.util.System.nanoTime()
+    long before = java.lang.System.nanoTime()
     task.run()
-    long after = java.util.System.nanoTime()
+    long after = java.lang.System.nanoTime()
     long took = TimeUnit.NANOSECONDS.toMillis(after - before)
     log.info "$name took $took ms."
   }
